5c77f9c3baec702441a7b550c9e3a450bd505ff8
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 03:10:03 +0100

    Create learnsets.ts

---
+++ data/mods/gen1rbyrecolored/learnsets.ts
@@ -0,0 +1,20 @@
+	venusaur: {
+		learnset: {
+			inherit: true,
+			stunspore: ["9L1"],
+		},
+	},
+	gyarados: {
+		learnset: {
+			inherit: true,
+			razorwind: ["9L1"],
+		},
+	},
+	arcanine: {
+		learnset: {
+			inherit: true,
+			thunderbolt: ["9L1"],
+			thunder: ["9L1"],
+			earthquake: ["9L1"],
+		},
+	},
