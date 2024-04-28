ed24626dc5078a94a0bd7686b9926e4c6f56c66e
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 05:07:26 +0100

    remove from learnsets.ts

--- data/mods/gen1rbyrecolored/learnsets.ts
+++ data/mods/gen1rbyrecolored/learnsets.ts
@@ -1,22 +1,2 @@
 export const Learnsets: {[k: string]: ModdedLearnsetData} = {
-	venusaur: {
-		learnset: {
-			inherit: true,
-			stunspore: ["1L1"],
-		},
-	},
-	gyarados: {
-		learnset: {
-			inherit: true,
-			razorwind: ["1L1"],
-		},
-	},
-	arcanine: {
-		learnset: {
-			inherit: true,
-			thunderbolt: ["1L1"],
-			thunder: ["1L1"],
-			earthquake: ["1L1"],
-		},
-	},
 }
