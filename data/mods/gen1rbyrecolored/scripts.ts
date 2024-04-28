d2abe0e54086abd1a0b7b4ad0803beb58ea4de60
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 03:17:31 +0100

    Update scripts.ts

--- data/mods/gen1rbyrecolored/scripts.ts
+++ data/mods/gen1rbyrecolored/scripts.ts
@@ -1,4 +1,14 @@
 export const Scripts: ModdedBattleScriptsData = {
     inherit: 'gen1',
     gen: 1,
+	 init() {
+		// Venusaur
+		this.modData("Learnsets", "venusaur").learnset.stunspore = ["1L1"];
+		// Gyarados
+		this.modData("Learnsets", "gyarados").learnset.razorwind = ["1L1"];
+		// Arcanine
+		this.modData("Learnsets", "arcanine").learnset.thunderbolt = ["1L1"];
+		this.modData("Learnsets", "arcanine").learnset.thunder = ["1L1"];
+		this.modData("Learnsets", "arcanine").learnset.earthquake = ["1L1"];
+	 },
 }
