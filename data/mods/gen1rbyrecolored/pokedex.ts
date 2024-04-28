db3dc6cf754b54bd8e104999d23b5a8df6570084
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 03:20:04 +0100

    Update pokedex.ts

--- data/mods/gen1rbyrecolored/pokedex.ts
+++ data/mods/gen1rbyrecolored/pokedex.ts
@@ -5,22 +5,16 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 	},
 	venusaur: {
 		inherit: true,
-		num: 1001,
-		species: "Venusaur",
 		types: ["Grass"],
 		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
 	},
 	gyarados: {
 		inherit: true,
-		num: 1002,
-		species: "Gyarados",
 		types: ["Water", "Flying"],
 		baseStats: {hp: 95, atk: 130, def: 79, spa: 105, spd: 105, spe: 81},
 	},
 	arcanine: {
 		inherit: true,
-		num: 1003,
-		species: "Arcanine",
 		types: ["Fire"],
 		baseStats: {hp: 95, atk: 125, def: 79, spa: 100, spd: 100, spe: 81},
 	},
