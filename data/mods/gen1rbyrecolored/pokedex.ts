b598ccd604faaaee3ad4cb37acb95eab1ea10c31
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 03:04:59 +0100

    venu gyara arc pokedex.ts

--- data/mods/gen1rbyrecolored/pokedex.ts
+++ data/mods/gen1rbyrecolored/pokedex.ts
@@ -3,4 +3,25 @@ export const Pokedex: {[k: string]: ModdedSpeciesData} = {
 		inherit: true,
 		baseStats: {hp: 55, atk: 50, def: 45, spa: 145, spd: 145, spe: 120},
 	},
+	venusaur: {
+		inherit: true,
+		num: 1001,
+		species: "Venusaur",
+		types: ["Grass"],
+		baseStats: {hp: 80, atk: 82, def: 83, spa: 100, spd: 100, spe: 80},
+	},
+	gyarados: {
+		inherit: true,
+		num: 1002,
+		species: "Gyarados",
+		types: ["Water", "Flying"],
+		baseStats: {hp: 95, atk: 130, def: 79, spa: 105, spd: 105, spe: 81},
+	},
+	arcanine: {
+		inherit: true,
+		num: 1003,
+		species: "Arcanine",
+		types: ["Fire"],
+		baseStats: {hp: 95, atk: 125, def: 79, spa: 100, spd: 100, spe: 81},
+	},
 }
