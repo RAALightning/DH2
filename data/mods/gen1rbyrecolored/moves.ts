d041b913b4644682a33ef408a03aea492d887755
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-04-28 01:57:48 +0100

    Update moves.ts

--- data/mods/gen1rbyrecolored/moves.ts
+++ data/mods/gen1rbyrecolored/moves.ts
@@ -1,3 +1,16 @@
 export const Moves: {[k: string]: ModdedMoveData} = {
-
+	dreameater: {
+		num: 138,
+		accuracy: 100,
+		basePower: 100,
+		category: "Physical",
+		name: "Dream Eater",
+		pp: 15,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, heal: 1, metronome: 1},
+		drain: [1, 2],
+		secondary: null,
+		target: "normal",
+		type: "Ghost",
+	},
 }
