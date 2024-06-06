c17f2aeefa27ba87b3036a00c39997e9a803ff3c
RAALightning <30796363+RAALightning@users.noreply.github.com>
2024-06-06 13:12:31 +0100

    update 2 real

---
+++ data/mods/gen1rbyrecolored/pokedex.ts
@@ -0,0 +1,91 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	aerodactyl: {
+		inherit: true,
+		types: ["Rock", "Flying"],
+		baseStats: {hp: 80, atk: 125, def: 65, spa: 60, spd: 60, spe: 130},
+	},
+	alakazam: {
+		inherit: true,
+		baseStats: {hp: 55, atk: 50, def: 45, spa: 145, spd: 145, spe: 120},
+	},
+	arcanine: {
+		inherit: true,
+		types: ["Fire"],
+		baseStats: {hp: 95, atk: 125, def: 79, spa: 100, spd: 100, spe: 81},
+	},
+	flareon: {
+		inherit: true,
+		types: ["Fire"],
+		baseStats: {hp: 95, atk: 130, def: 60, spa: 110, spd: 110, spe: 65},
+	},
+	golem: {
+		inherit: true,
+		types: ["Rock", "Ground"],
+		baseStats: {hp: 80, atk: 115, def: 140, spa: 65, spd: 65, spe: 45},
+	},
+	gyarados: {
+		inherit: true,
+		types: ["Water", "Flying"],
+		baseStats: {hp: 95, atk: 130, def: 79, spa: 105, spd: 105, spe: 81},
+	},
+	jolteon: {
+		inherit: true,
+		types: ["Electric"],
+		baseStats: {hp: 65, atk: 95, def: 60, spa: 110, spd: 110, spe: 130},
+	},
+	lapras: {
+		inherit: true,
+		types: ["Water", "Ice"],
+		baseStats: {hp: 150, atk: 80, def: 85, spa: 95, spd: 95, spe: 60},
+	},
+	magneton: {
+		inherit: true,
+		types: ["Electric"],
+		baseStats: {hp: 50, atk: 60, def: 180, spa: 120, spd: 120, spe: 50},
+	},
+	ninetales: {
+		inherit: true,
+		types: ["Fire", "Psychic"],
+		baseStats: {hp: 73, atk: 76, def: 75, spa: 100, spd: 100, spe: 100},
+	},
+	omastar: {
+		inherit: true,
+		types: ["Rock", "Water"],
+		baseStats: {hp: 70, atk: 60, def: 140, spa: 115, spd: 115, spe: 55},
+	},
+	onix: {
+		inherit: true,
+		types: ["Rock"],
+		baseStats: {hp: 120, atk: 85, def: 160, spa: 85, spd: 85, spe: 70},
+	},
+	raticate: {
+		inherit: true,
+		types: ["Normal"],
+		baseStats: {hp: 55, atk: 91, def: 60, spa: 65, spd: 65, spe: 105},
+	},
+	raichu: {
+		inherit: true,
+		types: ["Electric"],
+		baseStats: {hp: 70, atk: 100, def: 55, spa: 100, spd: 100, spe: 110},
+	},
+	tangela: {
+		inherit: true,
+		types: ["Grass", "Water"],
+		baseStats: {hp: 115, atk: 75, def: 95, spa: 95, spd: 95, spe: 45},
+	},
+	vaporeon: {
+		inherit: true,
+		types: ["Water"],
+		baseStats: {hp: 130, atk: 65, def: 60, spa: 110, spd: 110, spe: 95},
+	},
+	venusaur: {
+		inherit: true,
+		types: ["Grass"],
+		baseStats: {hp: 95, atk: 120, def: 83, spa: 100, spd: 100, spe: 81},
+	},
+	vileplume: {
+		inherit: true,
+		types: ["Grass", "Poison"],
+		baseStats: {hp: 95, atk: 75, def: 110, spa: 100, spd: 100, spe: 60},
+	},
+}
---
+++ data/mods/gen1rbyrecolored/scripts.ts
@@ -0,0 +1,74 @@
+export const Scripts: ModdedBattleScriptsData = {
+    inherit: 'gen1',
+    gen: 1,
+	 init() {
+		// Aerodactyl
+		this.modData("Learnsets", "aerodactyl").learnset.primalcry = ["1L1"];
+		this.modData("Learnsets", "aerodactyl").learnset.hurricane = ["1L1"];
+		this.modData("Learnsets", "aerodactyl").learnset.rockslide = ["1L1"];
+		// Arcanine
+		this.modData("Learnsets", "arcanine").learnset.thunderbolt = ["1L1"];
+		this.modData("Learnsets", "arcanine").learnset.thunderwave = ["1L1"];
+		this.modData("Learnsets", "arcanine").learnset.earthquake = ["1L1"];
+		// Articuno
+		this.modData("Learnsets", "articuno").learnset.primalcry = ["1L1"];
+		this.modData("Learnsets", "articuno").learnset.drillpeck = ["1L1"];
+		// Dragonite
+		this.modData("Learnsets", "dragonite").learnset.primalcry = ["1L1"];
+		// Exeggutor
+		this.modData("Learnsets", "exeggutor").learnset.rockslide = ["1L1"];
+		// Flareon
+		this.modData("Learnsets", "flareon").learnset.jumpkick = ["1L1"];
+		// Golem
+		this.modData("Learnsets", "golem").learnset.agility = ["1L1"];
+		this.modData("Learnsets", "golem").learnset.glare = ["1L1"];
+		this.modData("Learnsets", "golem").learnset.rollout = ["1L1"];
+		// Gyarados
+		this.modData("Learnsets", "gyarados").learnset.hurricane = ["1L1"];
+		// Lapras
+		this.modData("Learnsets", "lapras").learnset.primalcry = ["1L1"];
+		// Moltres
+		this.modData("Learnsets", "moltres").learnset.flamethrower = ["1L1"];
+		this.modData("Learnsets", "moltres").learnset.primalcry = ["1L1"];
+		this.modData("Learnsets", "moltres").learnset.drillpeck = ["1L1"];
+		//	Magneton
+		this.modData("Learnsets", "magneton").learnset.zapcannon = ["1L1"];
+		this.modData("Learnsets", "magneton").learnset.recover = ["1L1"];
+		// Ninetales
+		this.modData("Learnsets", "ninetales").learnset.thunderbolt = ["1L1"];
+		this.modData("Learnsets", "ninetales").learnset.psychic = ["1L1"];
+		this.modData("Learnsets", "ninetales").learnset.hypnosis = ["1L1"];
+		this.modData("Learnsets", "ninetales").learnset.recover = ["1L1"];
+		this.modData("Learnsets", "ninetales").learnset.willowisp = ["1L1"];
+		//	Omastar
+		this.modData("Learnsets", "omastar").learnset.rollout = ["1L1"];
+		this.modData("Learnsets", "omastar").learnset.recover = ["1L1"];
+		// Onix
+		this.modData("Learnsets", "onix").learnset.fireblast = ["1L1"];
+		this.modData("Learnsets", "onix").learnset.rollout = ["1L1"];
+		// Raichu
+		this.modData("Learnsets", "raichu").learnset.jumpkick = ["1L1"];
+		// Raticate
+		this.modData("Learnsets", "raticate").learnset.counter = ["1L1"];
+		// Tangela
+		this.modData("Learnsets", "tangela").learnset.surf = ["1L1"];
+		// Vaporeon
+		this.modData("Learnsets", "vaporeon").learnset.recover = ["1L1"];
+		// Venusaur
+		this.modData("Learnsets", "venusaur").learnset.primalcry = ["1L1"];
+		this.modData("Learnsets", "venusaur").learnset.stunspore = ["1L1"];
+		this.modData("Learnsets", "venusaur").learnset.blizzard = ["1L1"];
+		this.modData("Learnsets", "venusaur").learnset.icebeam = ["1L1"];
+		this.modData("Learnsets", "venusaur").learnset.earthquake = ["1L1"];
+		this.modData("Learnsets", "venusaur").learnset.rockslide = ["1L1"];
+		//	Victreebel
+		this.modData("Learnsets", "victreebel").learnset.earthquake = ["1L1"];
+		//	Vileplume
+		this.modData("Learnsets", "vileplume").learnset.amnesia = ["1L1"];
+		this.modData("Learnsets", "vileplume").learnset.blizzard = ["1L1"];
+		this.modData("Learnsets", "vileplume").learnset.icebeam = ["1L1"];
+		// Zapdos
+		this.modData("Learnsets", "zapdos").learnset.primalcry = ["1L1"];
+
+	 },
+}
